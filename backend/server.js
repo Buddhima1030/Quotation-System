1ff68fb457631bfd59b387c35683daed6f85046f
Buddhima1030 <buddhimajothiwansa123@gmail.com>
2026-06-09 23:30:54 +0530

    finished

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
-require("dotenv").config();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://quotation-system-eta-seven.vercel.app"
+];
 
-const express = require("express");
-const connectDB = require("./db");
-
-const customerRoutes = require("./routes/customerRoutes");
-const quotationRoutes = require("./routes/quotationRoutes");
-
-const app = express();
-
-connectDB();
-
-// Manual CORS fix
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
@@ -22,22 +19,3 @@ app.use((req, res, next) => {
 
   next();
 });
-
-app.use(express.json());
-
-app.use("/api/customers", customerRoutes);
-app.use("/api/quotations", quotationRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Quotation API Running");
-});
-
-const PORT = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-module.exports = app;
